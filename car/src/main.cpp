7ed576782e4553df1ff4f0e7a4e2ec948a19a05d
matejhumlicek <matmine.omg@gmail.com>
2019-11-21 20:51:24 +0100

    Update main.cpp

--- car/src/main.cpp
+++ car/src/main.cpp
@@ -1,8 +1,7 @@
-//This program is used to control a robot car or similar using a app that communicates with Arduino through a bluetooth module
+
 
 #include <Arduino.h>
 #include <AFMotor.h>
-//creates two objects to control two terminals on the motor shield 
 AF_DCMotor motor1(2,MOTOR12_1KHZ); 
 AF_DCMotor motor2(1,MOTOR12_1KHZ); 
 
@@ -10,15 +9,13 @@ char command;
 
 void setup() 
 {       
-  Serial.begin(9600);  //Set the baud rate to your Bluetooth module.
+  Serial.begin(9600); 
 }
 
 void loop(){
   if(Serial.available() > 0){ 
     command = Serial.read(); 
-    Stop(); //initialize with motors stopped
-    //Change pin mode only if new command is different from previous.   
-    //Serial.println(command);
+    Stop();    
     switch(command){
     case 'F':  
       forward();
@@ -41,41 +38,41 @@ void loop(){
 
 void forward()
 {
-  motor1.setSpeed(220); //Define maximum velocity
-  motor1.run(BACKWARD); //rotate the motor clockwise
-  motor2.setSpeed(220); //Define maximum velocity
-  motor2.run(FORWARD); //rotate the motor clockwise
+  motor1.setSpeed(220); 
+  motor1.run(BACKWARD); 
+  motor2.setSpeed(220);
+  motor2.run(FORWARD); 
 }
 
 void back()
 {
   motor1.setSpeed(220); 
-  motor1.run(FORWARD); //rotate the motor counterclockwise
+  motor1.run(FORWARD); 
   motor2.setSpeed(220); 
-  motor2.run(BACKWARD); //rotate the motor counterclockwise
+  motor2.run(BACKWARD); 
 }
 
 void left()
 {
-  motor1.setSpeed(220); //Define maximum velocity
-  motor1.run(FORWARD); //rotate the motor clockwise
+  motor1.setSpeed(220); 
+  motor1.run(FORWARD); 
   motor2.setSpeed(100);
-  motor2.run(FORWARD); //turn motor2 off
+  motor2.run(FORWARD); 
 }
 
 void right()
 {
   motor1.setSpeed(100);
-  motor1.run(FORWARD); //turn motor1 off
-  motor2.setSpeed(220); //Define maximum velocity
-  motor2.run(FORWARD); //rotate the motor clockwise
+  motor1.run(FORWARD); 
+  motor2.setSpeed(220); 
+  motor2.run(FORWARD); 
 }
 
 void Stop()
 {
   motor1.setSpeed(0);
-  motor2.run(RELEASE); //turn motor1 off
+  motor2.run(RELEASE); 
   motor2.setSpeed(0);
-  motor2.run(RELEASE); //turn motor2 off
+  motor2.run(RELEASE); 
 }
 
